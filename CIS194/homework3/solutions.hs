7f1fa30c5b163a9412e6d327b3ac2a68bd72c18c
Sahil Singh <sahilsingh90@gmail.com>
2017-05-07 01:33:15 +0530

    Added hw3, ex3 solutions. [Haskell]

--- CIS194/homework3/solutions.hs
+++ CIS194/homework3/solutions.hs
@@ -23,3 +23,28 @@ localMaxima (x : t@(y:z:xs))
 localMaxima _ = []
 
 -- End Exercise 2
+
+
+-- Exercise 3
+elemCounts :: [Int] -> Map.Map Int Int
+elemCounts = Map.fromListWith (\x y -> x + y) . map (\x -> (x,1))
+
+nLines :: Map.Map Int Int -> Int
+nLines = Map.foldlWithKey (\acc k v -> if acc > v then acc else v) 0
+
+buildLines 0 _ = map (\x -> '=') [0..9] ++ "\n" ++ 
+                 map intToDigit  [0..9] ++ "\n"
+buildLines n m = foldl (\acc x -> 
+                            if Map.member x $ filterMap n m 
+                                then acc ++ "*"
+                                else acc ++ " ") "" [0..9] ++ 
+                  "\n" ++ buildLines (n-1) m
+    where filterMap el mp = Map.filter (\v -> v >= el) mp
+
+histogram :: [Int] -> String
+histogram lst = buildLines n m
+    where
+        m = elemCounts lst
+        n = nLines m
+
+-- End Exercise 3
