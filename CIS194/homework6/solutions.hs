13a5aae200dcb8b8a9a9e38b59640fbaf73a75e8
Sahil Singh <sahilsingh90@gmail.com>
2017-05-14 02:10:47 +0530

    Solutions for Homework 6. [Haskell]

--- CIS194/homework6/solutions.hs
+++ CIS194/homework6/solutions.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 -- Exercise 1
 fib :: Integer -> Integer
 fib 0 = 0
@@ -56,7 +58,6 @@ streamFromSeed f a = Cons a (buildStream a)
 nats :: Stream Integer
 nats = streamFromSeed (+1) 0
 
-
 interleaveStreams :: Stream a -> Stream a -> Stream a
 interleaveStreams (Cons x stream1) (Cons y stream2) = 
             Cons x (Cons y (interleaveStreams stream1 stream2))
@@ -68,5 +69,53 @@ ruler = interleaveStreams (streamRepeat 0) (streamMap func $ streamFromSeed (+2)
         func = foldr (\x acc -> acc + 1) 0 . takeWhile even . iterate (`quot` 2)
 
 -- End Exercise 5
+
+
+-- Exercise 6
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+mulByInt n (Cons x stream) = Cons (n*x) $ mulByInt n stream
+addByInt n (Cons x stream) = Cons (n+x) stream
+
+instance Num (Stream Integer) where
+    fromInteger x = Cons x (streamFromSeed (*x) (0*x))
+    negate (Cons x stream) = Cons (-x) (negate stream)
+    (+) (Cons x (stream1)) (Cons y (stream2)) = 
+                                Cons (x + y) (stream1 + stream2)
+    (*) (Cons x (stream1)) s2@(Cons y (stream2)) = 
+                                Cons (x * y) ((mulByInt x stream2) + (stream1 * s2))
+
+instance Fractional (Stream Integer) where
+    (/) (Cons a (stream1)) (Cons b (stream2)) = q
+        where q = Cons (div a b) $ mulByInt (div 1 b) (stream1 - q * stream2)
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x * x)
+
+-- End Exercise 6
+
+
+-- Exercise 7
+
+data Matrix4 = Matrix4 Integer Integer Integer Integer 
+    deriving (Show)
+
+instance Num Matrix4 where
+    fromInteger x
+        | x == 1    = Matrix4 1 1 1 0
+        | otherwise = fromInteger 1 ^ x
+    (*) (Matrix4 a1 b1 c1 d1) (Matrix4 a2 b2 c2 d2) = 
+        Matrix4 ((a1 * a2) + (b1 * c2)) ((a1 * b2) + (b1 * d2))
+                ((c1 * a2) + (d1 * c2)) ((c1 * b2) + (d1 * d2))
+
+fib4 :: Integer -> Integer
+fib4 x
+    | x == 0    = 0
+    | otherwise = extractN $ fromInteger x
+    where extractN (Matrix4 _ _ n _) = n
+
+
+-- End Exercise 7
 
 
